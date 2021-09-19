68fa0db247013f22652be67c1a08f4e9782b7f31
shota_kizawa <shomok0106k@gmail.com>
2021-09-19 17:46:51 +0900

    Updates

---
+++ precache-manifest.82aa4c0f08397b9042931a4ac74acb61.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea40c7d89b8fc22a4eb",
+    "url": "/bingo-app-v2/css/app.ee2b15b5.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/bingo-app-v2/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/bingo-app-v2/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/bingo-app-v2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "53dca3b8207a301c33547f671479d0ad",
+    "url": "/bingo-app-v2/index.html"
+  },
+  {
+    "revision": "aea40c7d89b8fc22a4eb",
+    "url": "/bingo-app-v2/js/app.e7074212.js"
+  },
+  {
+    "revision": "90c758dcf42bc0ee31db",
+    "url": "/bingo-app-v2/js/chunk-vendors.1d8c27fa.js"
+  },
+  {
+    "revision": "1ea71744bb8befea9ab22a033da97f3f",
+    "url": "/bingo-app-v2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bingo-app-v2/robots.txt"
+  }
+]);
